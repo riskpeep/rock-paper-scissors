92ca6094077593a5b607ff01f9df0f98adc681f9
Robert Kerr <rskerr@gmail.com>
2022-07-10 20:02:36 -0400

    Selecting a Choice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+//
+// Rock, Paper, Scissors
+//
+// A game by Riskpeep
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Lets play Rock, Paper, Scissors!");
+
+    println!("Please select (r)ock, (p)aper, or (s)issors:");
+
+    let mut player_move = String::new();
+
+    io::stdin()
+        .read_line(&mut player_move)
+        .expect("Failed to read move");
+
+    println!("You guessed: {player_move}");
 }
